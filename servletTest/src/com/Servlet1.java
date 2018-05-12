a55f330cb2a01f9cb098b27b526c8670c1d029dc
xhy <123>
2018-05-12 16:52:54 +0800

    第3次提交

--- servletTest/src/com/Servlet1.java
+++ servletTest/src/com/Servlet1.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 public class Servlet1 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int x=99;
         System.out.println("你好！！！");
         System.out.println("提交到git中");
         resp.getWriter().write("你好！！！");
